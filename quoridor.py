7f7f8a9d53a5cce7625c117fa054d79702a636f8
Lee, Jun Seok <lego3410@gmail.com>
2023-04-02 17:35:24 +0900

    Update quoridor.py

--- quoridor.py
+++ quoridor.py
@@ -1,4 +1,9 @@
-import numpy as np
+from collections import deque
+
+# import numpy as np
+import torch
+import torch.nn.functional as F
+
 
 class QuoridorEnv:
     def __init__(self):
@@ -7,189 +12,251 @@ class QuoridorEnv:
         self.reset()
 
     def reset(self):
-        self.board = np.zeros((self.board_size, self.board_size), dtype=int)
-        self.fences = np.zeros((self.board_size - 1, self.board_size - 1), dtype=int)
-        self.player_positions = [((self.board_size - 1) // 2, 0), ((self.board_size - 1) // 2, self.board_size - 1)]
-        self.player_fences = [self.num_fences, self.num_fences]
+        self.board = torch.zeros(self.board_size, self.board_size, dtype=torch.int8)
+        self.fences = torch.zeros(
+            (self.board_size - 1, self.board_size - 1), dtype=torch.int8
+        )
+        self.player_positions = [
+            ((self.board_size - 1) // 2, 0),
+            ((self.board_size - 1) // 2, self.board_size - 1),
+        ]
+        self.fence_counts = [self.num_fences, self.num_fences]
         self.current_player = 0
+        self.board[self.player_positions[0]] = 1
+        self.board[self.player_positions[1]] = 2
         self.done = False
-        self.winner = None
         return self.get_state()
 
     def get_state(self):
-        return self.board.copy(), self.fences.copy(), tuple(self.player_positions), tuple(self.player_fences), self.current_player
+        # Returns the state as a tuple containing the current player,
+        # the board, the fence positions, and the remaining fence counts
+        return self.current_player, self.board, self.fences, tuple(self.fence_counts)
 
-    def is_valid_move(self, move):
-        x1, y1 = self.player_positions[self.current_player]
-        x2, y2 = move
+    def move_pawn(self, player, new_position):
+        if self.is_valid_move(player, new_position):
+            self.board[self.player_positions[player]] = 0
+            self.board[new_position] = player + 1
+            self.player_positions[player] = new_position
+            return True
+        return False
 
-        if not (0 <= x2 < self.board_size and 0 <= y2 < self.board_size):
+    def place_fence(self, player, fence_position, fence_orientation):
+        if self.is_valid_fence_placement(player, fence_position, fence_orientation):
+            x, y = fence_position
+            if fence_orientation == "h":
+                self.fences[x, y] |= 1
+            else:
+                self.fences[x, y] |= 2
+            self.fence_counts[player] -= 1
+            return True
+        return False
+
+    def step(self, action):
+        # action is a tuple: (action_type, action_data)
+        # action_type is either 'move' or 'fence'
+        # action_data is either a new_position for 'move' or (fence_position, fence_orientation) for 'fence'
+        action_type, action_data = action
+
+        if action_type == "move":
+            if self.move_pawn(self.current_player, action_data):
+                if self.has_won(self.current_player):
+                    self.done = True
+                else:
+                    self.current_player = 1 - self.current_player
+        elif action_type == "fence":
+            if self.place_fence(self.current_player, *action_data):
+                self.current_player = 1 - self.current_player
+
+    # Add methods for checking valid moves, fence placements, winning conditions, etc.
+    # These methods should include `is_valid_move`, `is_valid_fence_placement`, `has_won`, and any other
+    # required helper functions to determine the legality of actions and the game state.
+
+    def is_blocked(self, position, direction):
+        x, y = position
+        dx, dy = direction
+        nx, ny = x + dx, y + dy
+
+        assert 0 <= x < self.board_size and 0 <= y < self.board_size
+        assert abs(dx) + abs(dy) == 1
+
+        if not (0 <= nx < self.board_size and 0 <= ny < self.board_size):
+            return True
+
+        if dx != 0:
+            if y < self.board_size - 1 and self.fences[min(x, x + dx), y] & 2 != 0:
+                return True
+            if y > 0 and self.fences[min(x, x + dx), y - 1] & 2 != 0:
+                return True
+        else:
+            if x < self.board_size - 1 and self.fences[x, min(y, y + dy)] & 1 != 0:
+                return True
+            if x > 0 and self.fences[x - 1, min(y, y + dy)] & 1 != 0:
+                return True
+
+        return False
+
+    # Check if the given move is valid
+    def is_valid_move(self, player, new_position, check_opponent=True):
+        opponent = 2 - player
+        x, y = self.player_positions[player]
+        nx, ny = new_position
+        dx, dy = nx - x, ny - y
+
+        if not (0 <= nx < self.board_size and 0 <= ny < self.board_size):
             return False
 
-        dx, dy = x2 - x1, y2 - y1
-        if abs(dx) + abs(dy) > 2:
+        if abs(dx) > 2 or abs(dy) > 2:
             return False
 
-        opponent_pos = self.player_positions[1 - self.current_player]
-        if abs(dx) + abs(dy) == 1:  # Normal move or move after jumping over the opponent's pawn
-            if x1 == opponent_pos[0] and y1 == opponent_pos[1] - 1 and dy == 1:
-                if self.fences[y1, x1] & 1:  # There's a fence behind the opponent's pawn
-                    return False
-            elif x1 == opponent_pos[0] and y1 == opponent_pos[1] + 1 and dy == -1:
-                if self.fences[y1 - 1, x1] & 1:
-                    return False
-            elif y1 == opponent_pos[1] and x1 == opponent_pos[0] - 1 and dx == 1:
-                if self.fences[y1, x1] & 2:
-                    return False
-            elif y1 == opponent_pos[1] and x1 == opponent_pos[0] + 1 and dx == -1:
-                if self.fences[y1, x1 - 1] & 2:
-                    return False
-            else:  # Normal move
-                if dx == 1:
-                    if self.fences[y1, x1] & 2:
-                        return False
-                    if 0 <= y1 - 1 < self.board_size - 1 and self.fences[y1 - 1, x1] & 2:
-                        return False
-                elif dx == -1:
-                    if self.fences[y1, x1 - 1] & 2:
-                        return False
-                    if 0 <= y1 - 1 < self.board_size - 1 and self.fences[y1 - 1, x1 - 1] & 2:
-                        return False
-                elif dy == 1:
-                    if self.fences[y1, x1] & 1:
-                        return False
-                    if 0 <= x1 - 1 < self.board_size - 1 and self.fences[y1, x1 - 1] & 1:
-                        return False
-                elif dy == -1:
-                    if self.fences[y1 - 1, x1] & 1:
-                        return False
-                    if 0 <= x1 - 1 < self.board_size - 1 and self.fences[y1 - 1, x1 - 1] & 1:
-                        return False
-
-        else:  # Jumping over the opponent's pawn
-            if (x1 + x2) // 2 != opponent_pos[0] or (y1 + y2) // 2 != opponent_pos[1]:
+        if abs(dx) + abs(dy) == 1:
+            if self.is_blocked((x, y), (dx, dy)):
                 return False
 
-            if dx == 2:
-                if self.fences[y1, x1] & 2 or self.fences[y1, x1 + 1] & 2:
+        if not check_opponent:
+            return True
+
+        if self.board[new_position] != 0:
+            return False
+
+        if abs(dx) == 2 or abs(dy) == 2:  # Jump over opponent's pawn
+            if self.board[(x + nx) // 2, (y + ny) // 2] != opponent:
+                return False
+            if self.is_blocked((x, y), (dx // 2, dy // 2)):
+                return False
+            if self.is_blocked((nx, ny), (-dx // 2, -dy // 2)):
+                return False
+
+        elif abs(dx) == 1 and abs(dy) == 1:  # Diagonal move
+            if self.board[x, ny] == opponent:
+                if self.is_blocked((x, y), (0, dy)):
                     return False
-            elif dx == -2:
-                if self.fences[y1, x1 - 1] & 2 or self.fences[y1, x1 - 2] & 2:
+                if self.is_blocked((x, ny), (dx, 0)):
                     return False
-            elif dy == 2:
-                if self.fences[y1, x1] & 1 or self.fences[y1 + 1, x1] & 1:
+                if not self.is_blocked((x, ny), (0, dy)):
                     return False
-            elif dy == -2:
-                if self.fences[y1 - 1, x1] & 1 or self.fences[y1 - 2, x1] & 1:
+                return True
+
+            elif self.board[nx, y] == opponent:
+                if self.is_blocked((x, y), (dx, 0)):
                     return False
+                if self.is_blocked((nx, y), (0, dy)):
+                    return False
+                if not self.is_blocked((nx, y), (dx, 0)):
+                    return False
+                return True
+
+            return False
 
         return True
 
-    def is_valid_fence_placement(self, fence_pos, orientation):
-        x, y = fence_pos
+    # Check if the given fence placement is valid
+    def is_valid_fence_placement(self, player, fence_position, fence_orientation):
+        if self.fence_counts[player] == 0:
+            return False
+
+        x, y = fence_position
+
         if not (0 <= x < self.board_size - 1 and 0 <= y < self.board_size - 1):
             return False
 
-        if self.player_fences[self.current_player] == 0:
+        if self.fences[x, y] != 0:
             return False
 
-        if orientation == 0:
-            if self.fences[y, x] & 1:
-                return False
-            if x > 0 and self.fences[y, x - 1] & 1:
-                return False
-            if x < self.board_size - 2 and self.fences[y, x + 1] & 1:
+        if fence_orientation == "h":
+            if (
+                self.fences[x, y] & 1 != 0
+                or (x > 0 and self.fences[x - 1, y] & 1 != 0)
+                or (x < self.board_size - 2 and self.fences[x + 1, y] & 1 != 0)
+            ):
                 return False
         else:
-            if self.fences[y, x] & 2:
+            if (
+                self.fences[x, y] & 2 != 0
+                or (y > 0 and self.fences[x, y - 1] & 2 != 0)
+                or (y < self.board_size - 2 and self.fences[x, y + 1] & 2 != 0)
+            ):
                 return False
-            if y > 0 and self.fences[y - 1, x] & 2:
-                return False
-            if y < self.board_size - 2 and self.fences[y + 1, x] & 2:
+
+        self.fences[x, y] |= 1 if fence_orientation == "h" else 2
+        paths_exist = self.paths_exist()
+        self.fences[x, y] &= ~(1 if fence_orientation == "h" else 2)
+
+        return paths_exist
+
+    # Check if there is a path from the player's current position to the opposite side
+    def paths_exist(self):
+        checked = []
+        for player in (0, 1):
+            print(player)
+            visited = torch.zeros((self.board_size, self.board_size), dtype=bool)
+            queue = deque([self.player_positions[player]])
+            visited[self.player_positions[player]] = True
+
+            while queue:
+                x, y = queue.popleft()
+
+                if player == 0 and y == self.board_size - 1 or player == 1 and y == 0:
+                    checked.append(player)
+                    break
+
+                for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                    nx, ny = x + dx, y + dy
+                    if not self.is_blocked((x, y), (dx, dy)) and not visited[nx, ny]:
+                        visited[nx, ny] = True
+                        queue.append((nx, ny))
+            else:
                 return False
 
         return True
 
-    def step(self, action):
-        if not self.done:
-            if len(action) == 2:  # Move pawn
-                if self.is_valid_move(action):
-                    # Update player position
-                    self.player_positions[self.current_player] = action
+    # Check if the player has reached the opposite side
+    def has_won(self, player):
+        _, y = self.player_positions[player]
+        return y == (self.board_size - 1 if player == 0 else 0)
 
-                    # Check if player has reached the opposite side
-                    if (self.current_player == 0 and action[1] == self.board_size - 1) or (
-                            self.current_player == 1 and action[1] == 0):
-                        self.done = True
-                        self.winner = self.current_player
+    def is_game_over(self):
+        for player in range(2):
+            if self.has_won(player):
+                return True
+        return False
 
-            elif len(action) == 3:  # Place fence
-                x, y, orientation = action
-                fence_pos = y, x
-                if self.is_valid_fence_placement(fence_pos, orientation):
-                    # Update fence
-                    if orientation == 0:  # horizontal
-                        self.fences[fence_pos] = 1
-                    else:  # vertical
-                        self.fences[fence_pos] = 2
+    def get_winner(self):
+        for player in range(2):
+            if self.has_won(player):
+                return player
+        return None
 
-                    # Decrease the number of fences remaining
-                    self.player_fences[self.current_player] -= 1
+    def print_board(self):
+        h_fences = F.pad(self.fences, (0, 0, 1, 1)) & 1
+        v_fences = F.pad(self.fences, (1, 1, 0, 0)) & 2
 
-            # Switch to the next player
-            self.current_player = 1 - self.current_player
+        h_fences = h_fences[1:, :] | h_fences[:-1, :]
+        v_fences = v_fences[:, 1:] | v_fences[:, :-1]
 
-        return self.get_state(), self.done
+        for y in range(self.board_size):
+            row = []
+            for x in range(self.board_size):
+                if self.board[x, y] == 1:
+                    row.append("1")
+                elif self.board[x, y] == 2:
+                    row.append("2")
+                else:
+                    row.append(" ")
+                if x < self.board_size - 1:
+                    row.append("|" if v_fences[x, y] else " ")
+            print(" ".join(row))
+            if y < self.board_size - 1:
+                fence_row = []
+                for x in range(self.board_size):
+                    fence_row.append("-" if h_fences[x, y] else " ")
+                    if x < self.board_size - 1:
+                        fence_row.append("+")
+                print(" ".join(fence_row))
+        print()
 
-def print_state(state):
-    board, fences, player_positions, player_fences, current_player = state
-    print("Board:")
-    for y in range(board.shape[0]):
-        row = []
-        for x in range(board.shape[1]):
-            if (x, y) == player_positions[0]:
-                row.append("P1")
-            elif (x, y) == player_positions[1]:
-                row.append("P2")
-            else:
-                row.append("--")
-        print(" ".join(row))
-    print("\nFences:")
-    print(fences)
-    print("\nPlayer positions:", player_positions)
-    print("Player fences:", player_fences)
-    print("Current player:", current_player + 1)
 
-def parse_action(action_str):
-    parts = action_str.strip().split()
-    if len(parts) == 2:
-        x, y = int(parts[0]), int(parts[1])
-        return x, y
-    elif len(parts) == 3:
-        x, y, o = int(parts[0]), int(parts[1]), int(parts[2])
-        return x, y, o
-    else:
-        raise ValueError("Invalid action format")
-
+# Usage example:
 if __name__ == "__main__":
     env = QuoridorEnv()
-    state = env.reset()
-    print_state(state)
-
-    while not env.done:
-        # try:
-        action_str = input("Enter your action (x y) for move or (x y o) for fence placement: ")
-        action = parse_action(action_str)
-        if len(action) == 2 and not env.is_valid_move(action):
-            print("Invalid move. Please try again.")
-        elif len(action) == 3 and not env.is_valid_fence_placement(action[:-1], action[-1]):
-            print("Invalid move. Please try again.")
-        else:
-            state, done = env.step(action)
-            print_state(state)
-        # except ValueError as e:
-        #     print(e)
-        # except Exception as e:
-        #     print("Error:", e)
-        #     break
-    print("Game over! Winner is Player", env.winner + 1)
+    env.place_fence(0, (4, 4), "h")
+    env.print_board()
