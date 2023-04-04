f2e3d8adb1bf4c2a359be5ee37c9f4bbf7b55d1a
Lee, Jun Seok <lego3410@gmail.com>
2023-04-05 00:23:15 +0900

    Add replay_buffer.py

---
+++ replay_buffer.py
@@ -0,0 +1,20 @@
+import random
+
+
+class ReplayBuffer:
+    def __init__(self, capacity=10000):
+        self.capacity = capacity
+        self.buffer = []
+
+    def add(self, state, action, reward, next_state, done):
+        if len(self.buffer) >= self.capacity:
+            self.buffer.pop(0)
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+        return states, actions, rewards, next_states, dones
+
+    def __len__(self):
+        return len(self.buffer)
